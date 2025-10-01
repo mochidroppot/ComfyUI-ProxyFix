1905262bec7fb31160a45730b7ee9649b68414a3
mochidroppot <mochidroppot@gmail.com>
2025-10-01 10:20:43 +0900

    init commit

---
+++ web/__init__.py
@@ -0,0 +1,13 @@
+"""
+ComfyUI ProxyFix Web Extension
+
+This directory contains JavaScript files that will be automatically loaded
+by ComfyUI's web interface.
+
+Files:
+- fix-workflow-slash.js: Fixes the workflow path encoding issue
+"""
+
+# This file marks the web directory as a Python package.
+# The JavaScript files in this directory will be automatically loaded by ComfyUI.
+WEB_DIRECTORY = "."
